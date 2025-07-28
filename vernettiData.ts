32b813417b8a13f73970c6126601bf928004c21f
Gavin Dillon <rowan.j.dillon@gmail.com>
2025-07-28 13:55:24 -0700

    Updated Vernetti data ts

---
+++ vernettiData.ts
@@ -0,0 +1,193 @@
+export const vernettiData = {
+    parents: [
+      {
+        name: "John P. Vernetti",
+        birth: "27 Aug 1881, Torino, Piemonte, Italy",
+        death: "13 Apr 1966 Galen, Montana",
+        imageSrc: "/assets/Vernetti/John P. Vernetti.jpeg",
+      },
+      {
+        name: "Maria Picchioldi Vernetti",
+        birth: "2 Mar 1892, Italy",
+        death: "21 Apr 1969, Butte, Montana",
+        imageSrc: "/assets/Vernetti/Maria Picchioldi Vernetti.jpeg",
+      },
+    ],
+    children: [
+      {
+        parents: [
+          {
+            name: "Batt J. Vernetti",
+            birth: "14 Mar 1913, Butte, Montana",
+            death: "28 Apr 2000, Waco, Texas",
+            imageSrc: "/assets/Vernetti/Grandpa Vernetti.jpg",
+          },
+          {
+            name: "Anna Theresa Grunenfelder Vernetti",
+            birth: "04 Mar 1915, Mandan, North Dakota",
+            death: "28 Apr 2000, Waco, Texas",
+            imageSrc: "/assets/Vernetti/Grandma Vernetti.jpg",
+            marriageDate: "18 Apr 1940",
+          },
+          {
+            name: "Donald Leroy Marshall",
+            birth: "24 Jan 1912, Niles, Michigan",
+            death: "19 Jan 1998, South Bend, Indiana",
+            imageSrc: "/assets/Vernetti/Grandpa Marshall.jpg",
+          },
+          {
+            name: "Mary Kuzmitz Marshall",
+            birth: "02 Nov 1915, South Bend, Indiana",
+            death: "1 Jan 1996, South Bend, Indiana",
+            imageSrc: "/assets/Vernetti/Grandma Marshall.jpg",
+            marriageDate: "09 Sep 1944",
+          },
+        ],
+        children: [
+          {
+            parents: [
+              {
+                name: "James G. Vernetti",
+                birth: "02 Jun 1945, Dennison, Texas",
+                death: "15 May 2020",
+                imageSrc: "/assets/Vernetti/Grandpa Jim.jpg",
+              },
+              {
+                name: "Joan K. Marshall",
+                birth: "16 Sep 1945, South Bend, Indiana",
+                death: "Living",
+                imageSrc: "/assets/Vernetti/Nana.jpg",
+                marriageDate: "17 Aug 1968",
+              },
+            ],
+            children: [
+              {
+                parents: [
+                  {
+                    name: "Gavin Patrick Dillon",
+                    birth: "22 Feb 1969, Silver Springs, Maryland",
+                    death: "Living",
+                    imageSrc: "/assets/Vernetti/Gavin.PNG",
+                  },
+                  {
+                    name: "Kathryn Ann Vernetti Dillon",
+                    birth: "24 Oct 1969, San Diego, California",
+                    death: "Living",
+                    imageSrc: "/assets/Vernetti/Kathy.PNG",
+                    marriageDate: "02 Nov 1996",
+                    marriagePlace: "South Bend, Indiana",
+                  },
+                ],
+                children: [
+                  {
+                    parents: [
+                      {
+                        name: "Keelin Ann Dillon",
+                        birth: "23 May 2000, Chicago, Illinois",
+                        death: "Living",
+                        married: "Single",
+                        imageSrc: "/assets/Vernetti/Keelin.PNG",
+                      },
+                    ],
+                    children: [],
+                  },
+                  {
+                    parents: [
+                      {
+                        name: "Rowan James Dillon",
+                        birth: "04 Jul 2001, Seattle, Washington",
+                        death: "Living",
+                        married: "Single",
+                        imageSrc: "/assets/Vernetti/Snapchat-1227678694.jpg",
+                      },
+                    ],
+                    children: [],
+                  },
+                  {
+                    parents: [
+                      {
+                        name: "Shea Kathleen Dillon",
+                        birth: "11 Jul 2004, Seattle, Washington",
+                        death: "Living",
+                        married: "Single",
+                        imageSrc: "/assets/Vernetti/Shea.PNG",
+                      },
+                    ],
+                    children: [],
+                  },
+                ],
+              },
+              {
+                parents: [
+                  {
+                    name: "Louis Rangel",
+                    birth: "29 Apr 1969, Los Angeles, California",
+                    death: "Living",
+                    imageSrc: "/assets/Vernetti/Uncle Louis.png",
+                  },
+                  {
+                    name: "Kristin Lynn Vernetti Rangel",
+                    birth: "29 Apr 1973, Escondido, California",
+                    death: "Living",
+                    imageSrc: "/assets/Vernetti/Aunt Kristi.png",
+                    marriageDate: "13 March 2004",
+                    marriagePlace: "San Diego, California",
+                  },
+                ],
+                children: [
+                  {
+                    parents: [
+                      {
+                        name: "Nicolina Marie Rangel",
+                        birth: "23 Oct 2013, Torrance, California",
+                        death: "Living",
+                        married: "Single",
+                        imageSrc: "/assets/Vernetti/Nico.png",
+                      },
+                    ],
+                    children: [],
+                  },
+                ],
+              },
+              {
+                parents: [
+                  {
+                    name: "Brian James Vernetti",
+                    birth: "12 Aug 1978, Escondido, California",
+                    death: "Living",
+                    imageSrc: "/assets/Vernetti/Uncle Brian.PNG",
+                  },
+                ],
+                children: [
+                  {
+                    parents: [
+                      {
+                        name: "Benjamin James Vernetti",
+                        birth: "08 Oct 2019 San Diego, California",
+                        death: "Living",
+                        married: "Single",
+                        imageSrc: "/assets/Vernetti/Ben.png",
+                      },
+                    ],
+                    children: [],
+                  },
+                  {
+                    parents: [
+                      {
+                        name: "Robert Blake Vernetti",
+                        birth: "14 Dec 2021 San Diego, California",
+                        death: "Living",
+                        married: "Single",
+                        imageSrc: "/assets/Vernetti/Blake.png",
+                      }
+                    ],
+                    children: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  }
